17dff2e7816e090233a17d7ce045f1dea1ced9fb
flaketill <armandoibarra1@gmail.com>
2023-08-09 23:12:29 -0600

    UPD: update project settings

--- backend/settings.py
+++ backend/settings.py
@@ -1,22 +1,9 @@
-import os
+from os import environ
+
+
 """Flask APP configuration."""
-from dotenv import load_dotenv
-from flask import current_app
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-import logging
-
-# Configure the logger
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-load_dotenv('.env') #the path to your .env file (or any other file of environment variables you want to load)
-
-
-MYSQL_HOST = os.getenv("MYSQL_HOST") # HOST
-MYSQL_USER = os.getenv("MYSQL_USER") # USER
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD") # PASS
-MYSQL_DB = os.getenv("MYSQL_DB") # DATABASE
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-
-SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+FLASK_ENV = environ.get('FLASK_ENV')
+SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+SHOW_SQLALCHEMY_LOG_MESSAGES = environ.get('SHOW_SQLALCHEMY_LOG_MESSAGES')
+SQLALCHEMY_ECHO = environ.get('SQLALCHEMY_ECHO')
